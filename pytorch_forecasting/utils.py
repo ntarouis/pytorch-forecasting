8dfad0e649f6f827c8db53289a9ff6fb194d3cdd
Eirini Nt <45299924+ntarouis@users.noreply.github.com>
2021-03-17 12:32:58 +0200

    Update utils.py

--- pytorch_forecasting/utils.py
+++ pytorch_forecasting/utils.py
@@ -31,10 +31,8 @@ def integer_histogram(
         min = uniques.min()
     if max is None:
         max = uniques.max()
-    max=uniques.max()
-    min=uniques.min()
-#     hist = torch.zeros(max - min + 1 , dtype=torch.long, device=data.device).scatter(dim=0, index=uniques - min, src=counts)
-#     return hist
+
+
 
 
 def groupby_apply(
